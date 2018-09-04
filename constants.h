2ce114c93f8f35f78ef3725e1c6ce79ce987e06c
ScienziatoBestia <bruno.zerbo@gmail.com>
2018-09-04 14:00:39 +0200

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+const TString CALIBRATIONFILE("calibration_file.root");     //DA Togliere
+const Double_t SAMPLING_RATE = 3.2;
+const Int_t EX_PEAKS= 16;
+
+
+const unsigned int kNTimeSamples = 1024;
+const unsigned int kNCh = 8;
+
+
+#endif // CONSTANTS_H
+
